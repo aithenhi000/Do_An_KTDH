5241acb25a16b49d790d1e61fa2eb145ca4d42c5
aithenhi000 <114046422+aithenhi000@users.noreply.github.com>
2024-05-30 00:36:47 +0700

    Update home.py

--- home.py
+++ home.py
@@ -15,6 +15,7 @@ class Home(Frame):
         self.create_grid_pixel()
         self.write_name()
         self.create_circle()
+        self.move_circle()
         
     def tinh_tien(self, pos, delta_x, delta_y):
 
@@ -24,17 +25,20 @@ class Home(Frame):
         pos = np.matmul(pos, mul_matrix)
 
         return pos    
-    
-    def create_circle(self):
-        pass
-        # pos1, p1 = self.draw_circle(0, 0, 50)
-        # pos1=self.tinh_tien(pos1, 0, 20)
-        # self.canvas.delete(*p1)
+
         
-        # pos1, p1 = self.draw_circle(pos1[0],pos1[1], 50)
-        # pos1=self.tinh_tien(pos1, 0, -20)
-        # pos1, p1 = self.draw_circle(pos1[0],pos1[1], 50)
-        # self.canvas.after(100, self.create_circle())
+        
+    def move_circle(self):
+        self.pos1=self.tinh_tien(self.pos1, 0, -5)    
+        print(self.pos1) 
+        self.canvas.delete(*self.p1)
+        self.pos1, self.p1 = self.draw_circle(self.pos1[0],self.pos1[1], 50)
+        
+        self.after(100, self.move_circle)
+        
+    def create_circle(self):
+        self.pos1, self.p1 = self.draw_circle(0, 80, 50)
+
     
     def write_name(self):
         center_x = self.width // 2
@@ -46,13 +50,7 @@ class Home(Frame):
         self.canvas = Canvas(self, width=self.width, height=self.height, bg="#FEFAF6")
         self.canvas.pack()
         
-    def create_grid_pixel(self):
-        canvas=self.canvas   
-        # draw grid
-        for x in range(0, self.width, self.grid_size):
-            canvas.create_line(x, 0, x, self.height, fill="#EADBC8")
-        for y in range(0, self.height, self.grid_size):
-            canvas.create_line(0, y, self.width, y, fill="#EADBC8")
+    
             
     def put_pixel(self, x, y, color="green"):
             adjusted_x = self.width/2 + x*5
@@ -90,29 +88,36 @@ class Home(Frame):
                 p = p + 2*y - 2*x + 1
             
         return np.array(([x_center, y_center, 1])), points
-        
-        # # X axis
-        # canvas.create_line(0, self.origin[1], self.width, self.origin[1])
-        # for x in range(0, self.width, 50):
-        #     canvas.create_rectangle(
-        #         x - 1, self.origin[1] - 1, x + 1, self.origin[1] + 1, fill="red"
-        #     )
-        #     canvas.create_text(
-        #         x,
-        #         self.origin[1] + 8,
-        #         text=str(int((x - self.width / 2) / 5)),
-        #         font=("Arial", 7),
-        #     )
+    
+    def create_grid_pixel(self):
+        canvas=self.canvas   
+        # draw grid
+        for x in range(0, self.width, self.grid_size):
+            canvas.create_line(x, 0, x, self.height, fill="#EADBC8")
+        for y in range(0, self.height, self.grid_size):
+            canvas.create_line(0, y, self.width, y, fill="#EADBC8")
+        # X axis
+        canvas.create_line(0, self.origin[1], self.width, self.origin[1])
+        for x in range(0, self.width, 50):
+            canvas.create_rectangle(
+                x - 1, self.origin[1] - 1, x + 1, self.origin[1] + 1, fill="red"
+            )
+            canvas.create_text(
+                x,
+                self.origin[1] + 8,
+                text=str(int((x - self.width / 2) / 5)),
+                font=("Arial", 7),
+            )
 
-        # # Y axis
-        # canvas.create_line(self.origin[0], 0, self.origin[0], self.height)
-        # for y in range(0, self.height, 50):
-        #     canvas.create_rectangle(
-        #         self.origin[0] - 1, y - 1, self.origin[0] + 1, y + 1, fill="red"
-        #     )
-        #     canvas.create_text(
-        #         self.origin[0] + 9,
-        #         y,
-        #         text=str(int(-(y - self.height / 2) / 5)),
-        #         font=("Arial", 7),
-        #     )
+        # Y axis
+        canvas.create_line(self.origin[0], 0, self.origin[0], self.height)
+        for y in range(0, self.height, 50):
+            canvas.create_rectangle(
+                self.origin[0] - 1, y - 1, self.origin[0] + 1, y + 1, fill="red"
+            )
+            canvas.create_text(
+                self.origin[0] + 9,
+                y,
+                text=str(int(-(y - self.height / 2) / 5)),
+                font=("Arial", 7),
+            )
