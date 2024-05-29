61521a3d11cf90ede456c142a7adc22da2a10680
phucphan01866 <phucphan01866@gmail.com>
2024-05-29 20:29:04 +0700

    Thêm tên thành viên

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ class Application(Tk):
         self.width=1500
         self.height=750
         self.title("ĐỒ ÁN KỸ THUẬT ĐỒ HỌA")
-        self.iconbitmap('Do_An_KTDH\\icon_app.ico')
+        self.iconbitmap('icon_app.ico')
         self.geometry(f"{self.width}x{self.height}")
         self.create_taskbar()
         self.state("zoomed")
         self.show_home_page()
-
+        
     
     def create_taskbar(self):
         # Tạo thanh taskbar
@@ -29,25 +29,21 @@ class Application(Tk):
         self.button_2d = Button(self.taskbar_frame, text="GRAPHICS 2D", command=self.show_2d_application, font=button_font, bg='#FFC470')
         self.button_2d.pack(side="left", padx=5)
 
-        
-        
         # Tạo button 3D
         self.button_3d = Button(self.taskbar_frame, text="GRAPHICS 3D", command=self.show_3d_application, font=button_font,bg='#4793AF')
         self.button_3d.pack(side="left", padx=5)
 
-
         self.lb_title=Label(self.taskbar_frame, text="", bg="lightgray", font=("Courier", 16, "bold"))
         self.lb_title.pack(side="left", padx=300)
         
-
-            
+                 
     def show_home_page(self):
         self.clear_application()
         self.home = Home(master=self, width=self.width, height=self.height)
         self.lb_title.config(text='ĐỒ ÁN KỸ THUẬT ĐỒ HỌA')
         self.home.pack()
         
-    
+        
     def show_2d_application(self):
         self.clear_application()
         self.app_2d = Graphics2D(master=self)
@@ -55,19 +51,20 @@ class Application(Tk):
         self.app_2d.pack()
         # self.sb1=sailboat(master=self)
         # self.sb1.pack()
+        
 
     def show_3d_application(self):
         self.clear_application()
         self.lb_title.config(text='GIAO DIỆN ĐỒ HỌA 3D')
-        self.app_3d = Graphics3D(master=self, width=self.width, height=self.height)
+        self.app_3d = Graphics3D(master=self)
         self.app_3d.pack()
+        
 
     def clear_application(self):
         for widget in self.winfo_children():
             if isinstance(widget, (Graphics3D, Graphics2D, Home)):
                 widget.pack_forget()
-    
-
+                
 
 def main():
     app = Application()
